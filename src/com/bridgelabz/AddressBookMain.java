1de3f07ffc3c631da05608d65b3d75bb8d310e36
MayaChandre <84507177+MayaChandre@users.noreply.github.com>
2021-07-16 16:23:48 +0530

    Update AddressBookMain.java

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -138,7 +138,7 @@ public static Scanner sc = new Scanner(System.in);
 	}
 
 	public static void main(String[] args) {
-		System.out.println("----------------****Welcome to the Address Book System****--------------");
+		System.out.println("Welcome to the Address Book System");
 		AddressBookMain addressBookMain = new AddressBookMain();
 		boolean flag = true;
 		while (flag) {
